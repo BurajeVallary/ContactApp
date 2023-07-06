2960462ab49ef95b98700053b82747c8cab20e8f
Vallary Buraje <vallaryburaje@gmail.com>
2023-07-06 08:40:46 +0300

    changes

--- app/src/main/java/dev/val/contactapp/MainActivity.kt
+++ app/src/main/java/dev/val/contactapp/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
        binding.tvlogin.setOnClickListener{
            openMainActivity2()
        }
